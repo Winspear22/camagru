e95369f816d8abcd6b2e738bf02dbf1ec80cf7da
Winspear22 <adnen.aloui2@hotmail.com>
2023-10-25 15:44:57 +0200

    Camagru se lance avec la ddb en conteneur seulement

--- src/nestjs/src/app.module.ts
+++ src/nestjs/src/app.module.ts
@@ -1,9 +1,36 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
+//@Module({
+//  imports: [TypeOrmModule.forRoot({
+//      type: 'postgres',
+//      host: process.env.POSTGRES_HOST || 'postgresql',
+//      port: parseInt(process.env.POSTGRES_PORT, 10) || 5432,
+//      username: process.env.POSTGRES_USER,
+//      password: process.env.POSTGRES_PASSWORD,
+//      database: process.env.POSTGRES_DB,
+//      entities: [],
+//      synchronize: true,
+//    })],
+//  controllers: [AppController],
+//  providers: [AppService],
+//})
+//export class AppModule {}
+
+// Qd NestJS est en dehors de contenur
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: '172.18.0.2',
+      port: 5432,
+      username: "user",
+      password: "1234",
+      database: "pong_db",
+      entities: [],
+      synchronize: true,
+    })],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/nestjs/src/main.ts
+++ src/nestjs/src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import * as dotenv from 'dotenv'; // Importez dotenv
+dotenv.config({ path: '/home/adnen/Desktop/camagru/.env' });
+
+
+dotenv.config();
+
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  console.log(process.env.POSTGRES_HOST);
+  console.log(process.env.POSTGRES_PORT);
+  console.log(process.env.POSTGRES_USER);
+  console.log(process.env.POSTGRES_PASSWORD);
+  console.log(process.env.POSTGRES_DB);
+  const myVariable = process.env.POSTGRES_DB;
+  console.log(myVariable);
+
   await app.listen(3000);
 }
 bootstrap();
